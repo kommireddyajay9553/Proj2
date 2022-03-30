8693fb56b5ef422b6bbc3f55b073170feab4692e
kommireddyajay9553 <kommireddyajay9553@gmail.com>
2022-03-30 10:08:29 +0530

    ajay is don changes

--- src/A.java
+++ src/A.java
@@ -2,6 +2,9 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello...");
+		
+		  System.out.println("Ajay is don...");
+		
 		System.out.println("Bye...");
 	}
 }
